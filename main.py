7b603f11b8747892514e321775fcc4c1d187c615
Zaiyancool <64119609+Zaiyancool@users.noreply.github.com>
2025-04-26 23:24:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+from keep_alive import keep_alive
+
+# Start Flask server
+keep_alive()
+
+# Telegram bot credentials
+BOT_TOKEN = BOT_TOKEN = os.environ['BOT_TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
+
+
+def send_poll():
+    now = datetime.now().strftime('%Y-%m-%d')
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPoll"
+
+    question = f"Mutabaah Amal ({now})"
+    options = ["Mathurat Pagi", "Mathurat Petang", "1 muka Al-Quran sehari", "Sedekah 1 minggu skali", "Ana tak sempat/terlupa"]
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "question": question,
+        "options": options,
+        "is_anonymous": False,   # Not anonymous
+        "allows_multiple_answers": True  # Can choose multiple
+    }
+
+    response = requests.post(url, json=payload)
+    print(response.json())
+
+# Scheduler
+scheduler = BlockingScheduler(timezone="Asia/Kuala_Lumpur")
+scheduler.add_job(send_poll, 'cron', hour=23, minute=0)  
+
+# Start
+scheduler.start()
